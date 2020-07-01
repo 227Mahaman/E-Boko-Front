a72db3ce21e0269541d8af2b7ab16083c026814d
227Mahaman <zalkiabass.456@gmail.com>
2020-07-02 00:38:56 +0100

    Génération des components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EtudiantComponent } from './etudiant/etudiant.component';
+import { UeComponent } from './ue/ue.component';
+import { AnneescolaireComponent } from './anneescolaire/anneescolaire.component';
+import { DevoirComponent } from './devoir/devoir.component';
+import { FiliereComponent } from './filiere/filiere.component';
+import { ModuleComponent } from './module/module.component';
+import { NiveauComponent } from './niveau/niveau.component';
+import { NoteComponent } from './note/note.component';
+import { SalleComponent } from './salle/salle.component';
+import { SemestreComponent } from './semestre/semestre.component';
+import { SessionComponent } from './session/session.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EtudiantComponent,
+    UeComponent,
+    AnneescolaireComponent,
+    DevoirComponent,
+    FiliereComponent,
+    ModuleComponent,
+    NiveauComponent,
+    NoteComponent,
+    SalleComponent,
+    SemestreComponent,
+    SessionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/devoir/devoir.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-devoir',
+  templateUrl: './devoir.component.html',
+  styleUrls: ['./devoir.component.css']
+})
+export class DevoirComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
