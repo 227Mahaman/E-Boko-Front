a5040fb192a11a579886774cd038586e48cbe01d
227Mahaman <zalkiabass.456@gmail.com>
2020-07-07 19:22:56 +0100

    Listing des devoirs

--- src/app/devoir/devoir.component.ts
+++ src/app/devoir/devoir.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Devoir} from "../models/devoir";
+import {ApiService} from "../shared/api.service";
+import {Module} from "../models/module";
 
 @Component({
   selector: 'app-devoir',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./devoir.component.css']
 })
 export class DevoirComponent implements OnInit {
+  devoirs: Devoir[] = [];
+  modules: Module[] = [];
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.getAllDevoirs();
+    this.getAllModules();
   }
 
+  getAllDevoirs(){
+    this.apiService.getAllDevoirs().subscribe(
+      res => {
+        this.devoirs = res;
+      },
+      err => {
+        alert("Erreur survenue lors de la recuperation des données de la table devoir !");
+      }
+    );
+  }
+
+  public getAllModules(){
+    this.apiService.getAllModules().subscribe(
+      res => {
+        this.modules = res;
+      },
+      err => {
+        alert("Erreur survenue lors de la recuperation des données de la table module !");
+      }
+    );
+  }
+
+  deleteDevoir(devoir: Devoir) {
+
+  }
+
+  createDevoir(event) {
+
+  }
 }
